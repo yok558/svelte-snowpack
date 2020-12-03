555d71f2e0a1d83a5573fa8a1f97e3380477fcf9
yok558 <yok.box@gmail.com>
2020-12-04 02:29:25 +0300

    customized project layout

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,11 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+   // directory name: 'build directory'
+   public: '/',
+   src: '/_dist_',
   },
   plugins: [
-    /* ... */
     '@snowpack/plugin-svelte'
   ],
   install: [
