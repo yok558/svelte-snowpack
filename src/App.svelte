02331e5d77f970216a94f93b007a10a193a1ea34
yok558 <yok.box@gmail.com>
2020-12-04 19:55:22 +0300

    added logo

--- src/App.svelte
+++ src/App.svelte
@@ -2,17 +2,42 @@
 <script>
   /* component logic will go here */
 </script>
+
 <style>
-  /* css will go here */
+  .App-header {
+    background-color: #f9f6f6;
+    color: #333;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+  }
+  .App-logo {
+    height: 36vmin;
+    pointer-events: none;
+    margin-bottom: 3rem;
+    animation: App-logo-pulse infinite 1.6s ease-in-out alternate;
+  }
+  @keyframes App-logo-pulse {
+    from {
+      transform: scale(1);
+    }
+    to {
+      transform: scale(1.06);
+    }
+  }
 </style>
+
 <div class="App">
   <header class="App-header">
+    <img src="/logo.svg" class="App-logo" alt="logo" />
     <a
       class="App-link"
       href="https://svelte.dev"
       target="_blank"
-      rel="noopener noreferrer"
-    >
+      rel="noopener noreferrer">
       Learn Svelte
     </a>
   </header>
